c60f03f356beb6e6d4b6d4fd9c16c591c3fbb8d4
Seunmul <geonhab504@gmail.com>
2022-08-10 04:06:31 +0900

    no headless mode

--- __sugang__.py
+++ __sugang__.py
@@ -17,8 +17,8 @@ prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2,
                                                     'protected_media_identifier': 2, 'app_banner': 2,
                                                     'site_engagement': 2,
                                                     'durable_storage': 2}}
-options.add_experimental_option('prefs', prefs)
-options.add_argument('headless')
+# options.add_experimental_option('prefs', prefs)
+# options.add_argument('headless')
 options.add_argument("start-maximized")
 options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
